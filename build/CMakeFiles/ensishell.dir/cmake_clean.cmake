1e3aedca0767c603a1d1e0c0edad21ac5b729121
obermania <emmanuel.obermeyer@gmail.com>
2018-11-05 19:30:36 +0100

    l

---
+++ build/CMakeFiles/ensishell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ensishell.dir/src/readcmd.c.o"
+  "CMakeFiles/ensishell.dir/src/ensishell.c.o"
+  "ensishell.pdb"
+  "ensishell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ensishell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
